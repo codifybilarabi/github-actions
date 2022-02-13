27895d795d07d08bb9d1bf37307a7ce281152c49
osama abo elkhair <osama.abulkhair@gmail.com>
2022-02-13 15:30:25 +0100

    first commit

---
+++ src/main/java/com/githubactions/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.githubactions.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+	@RestController
+	class controller {
+		@GetMapping
+		public String hello() {
+			return "Hello github actions!!!";
+		}
+	}
+}
